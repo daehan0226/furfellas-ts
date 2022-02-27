0c48e1938a9daad94baece65651e1d768a9af15b
daehan <dh16931@gmail.com>
2022-02-27 22:59:51 +0900

    feat: add alert for google login

--- src/components/member/SignIn.tsx
+++ src/components/member/SignIn.tsx
@@ -1,98 +1,128 @@
-import { useEffect, useState } from 'react';
-import { RouteComponentProps } from 'react-router-dom';
-import { useActions } from '../../hooks/useActions';
-import { useAppSelector } from '../../hooks/useTypedSelector';
+import { useEffect, useState } from "react";
+import { RouteComponentProps } from "react-router-dom";
+import styled from "styled-components";
+import { useActions } from "../../hooks/useActions";
+import { useAppSelector } from "../../hooks/useTypedSelector";
 import { Input, Button } from "../../components/common";
-import { message, Divider } from 'antd';
-import {GoogleLoginCompnent} from "./social";
+import { themeProps } from "../../styles/theme";
+import { message, Divider, Alert } from "antd";
+import { GoogleLoginCompnent } from "./social";
+import { browserName } from "react-device-detect";
 
 type LocationState = {
-    nextPage?: string;
-}
+  nextPage?: string;
+};
 
 type Mock = {
-    [key: string]: string | undefined;
-}
+  [key: string]: string | undefined;
+};
 
 type WithLocatonState = RouteComponentProps<Mock, Mock, LocationState>;
 
+const AlertBox = styled.main`
+  width: 280px;
+  margin: 20px auto;
+  display: none;
+
+  ${({ theme }: themeProps) => theme.media.phone`
+        display: block;
+    `}
+`;
 
 const SignIn: React.FC<WithLocatonState> = (props) => {
-    const [username, setUsername] = useState<string>("");
-    const [password, setPassword] = useState<string>("");
-    const [err, setErr] = useState<string>("");
-    const [submitDisabled, setSubmitDisabled] = useState<boolean>(true);
+  const [username, setUsername] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [err, setErr] = useState<string>("");
+  const [submitDisabled, setSubmitDisabled] = useState<boolean>(true);
 
-    const auth = useAppSelector((state) => state.auth);
-    const { authenticate } = useActions();
+  const auth = useAppSelector((state) => state.auth);
+  const { authenticate } = useActions();
 
-    const handleSubmit = () => {
-        authenticate({ username, password })
+  const handleSubmit = () => {
+    authenticate({ username, password });
+  };
+
+  const info = (name: string) => {
+    message.info(`Hi, ${name}`);
+  };
+
+  useEffect(() => {
+    if (auth.loggedIn && auth.data) {
+      info(auth.data.user);
+      if (props.location.state && props.location.state.nextPage) {
+        props.history.push(props.location.state.nextPage);
+      } else {
+        props.history.push("/");
+      }
     }
-
-    const info = (name: string) => {
-        message.info(`Hi, ${name}`);
-    };
-
-    useEffect(() => {
-        if (auth.loggedIn && auth.data) {
-            info(auth.data.user)
-            if (props.location.state && props.location.state.nextPage) {
-                props.history.push(props.location.state.nextPage)
-            } else {
-                props.history.push('/')
-            }
-        }
-        if (auth.error) {
-            setErr(auth.error)
-        } else {
-            setErr('')
-        }
-    }, [auth])
-
-    const validateInputs = () => {
-        setSubmitDisabled(true);
-
-        if (username !== "" && password !== "") {
-            setSubmitDisabled(false)
-        }
+    if (auth.error) {
+      setErr(auth.error);
+    } else {
+      setErr("");
     }
+  }, [auth]);
 
-    useEffect(() => {
-        setErr('')
-        validateInputs()
-    }, [username, password])
+  const validateInputs = () => {
+    setSubmitDisabled(true);
 
-    const handlePressEnter = () => {
-        if (!submitDisabled) {
-            handleSubmit()   
-        }
+    if (username !== "" && password !== "") {
+      setSubmitDisabled(false);
     }
+  };
 
-    return (
-        <>
-            <h1>Sign in</h1>
-            <Input
-                placeholder="username"
-                value={username}
-                onChange={(e) => { setUsername(e.target.value) }}
-                rules={[{ required: true, message: 'Please input your username!' }]}
-                autoFocus={true}
-            />
-            <Input
-                placeholder="password"
-                value={password}
-                onChange={(e) => { setPassword(e.target.value) }}
-                rules={[{ required: true, message: 'Please input your password!' }]}
-                type="password"
-                enterKeyCallback={()=>handlePressEnter()}
-            />
-            {err && (<p>{err}</p>)}
-            <Button text={"Login"} onClick={handleSubmit} disabled={submitDisabled} loading={auth.loading} />
-            <Divider />
-            <GoogleLoginCompnent clientId = {"478378904599-hsv46g4ebvq10u19jfh1unjs7irghniq.apps.googleusercontent.com"} />
-        </>
-    )
-}
+  useEffect(() => {
+    setErr("");
+    validateInputs();
+  }, [username, password]);
+
+  const handlePressEnter = () => {
+    if (!submitDisabled) {
+      handleSubmit();
+    }
+  };
+
+  return (
+    <>
+      <h1>Sign in</h1>
+      <Input
+        placeholder="username"
+        value={username}
+        onChange={(e) => {
+          setUsername(e.target.value);
+        }}
+        rules={[{ required: true, message: "Please input your username!" }]}
+        autoFocus={true}
+      />
+      <Input
+        placeholder="password"
+        value={password}
+        onChange={(e) => {
+          setPassword(e.target.value);
+        }}
+        rules={[{ required: true, message: "Please input your password!" }]}
+        type="password"
+        enterKeyCallback={() => handlePressEnter()}
+      />
+      {err && <p>{err}</p>}
+      <Button
+        text={"Login"}
+        onClick={handleSubmit}
+        disabled={submitDisabled}
+        loading={auth.loading}
+      />
+      <Divider />
+      <GoogleLoginCompnent />
+
+      {browserName !== "Chrome" && (
+        <AlertBox>
+          <Alert
+            message="Use Google Chrome to log in with Google"
+            type="warning"
+          />
+        </AlertBox>
+      )}
+    </>
+  );
+};
 
 export default SignIn;
